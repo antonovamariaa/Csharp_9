7aa2d0d16639f40a9ce9a1c7fee3c6d61a944f08
maria <antonova.maria.a@gmail.com>
2023-07-11 20:55:06 +0300

    task 1 done

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int Input(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int SumNums(int m, int n)
+{
+    if (m == n) { return m; }
+    return n + SumNums(m, n - 1);
+}
+
+//---------------------------------------------
+
+int m = Input("input first number: ");
+int n = Input("input second number: ");
+System.Console.WriteLine(SumNums(m, n));
