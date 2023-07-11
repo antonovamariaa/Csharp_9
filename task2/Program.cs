e9b71d0fc4186b7ebaa666715873dfa9b9274fe8
maria <antonova.maria.a@gmail.com>
2023-07-11 21:07:31 +0300

    task 2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Input(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int Function(int m, int n)
+{
+    if (m == 0) { return n + 1; }
+    if (n == 0) { return Function(m - 1, 1); }
+    return Function(m - 1, Function(m, n - 1));
+}
+
+//---------------------------------------------------------
+int m = Input("input first number: ");
+int n = Input("input second number: ");
+System.Console.WriteLine(Function(m, n));
+
+
+
+
+
